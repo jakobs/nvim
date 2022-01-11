7256020dd039109ddd996e38439c8a571750ced7
Jakob Schwendner <jschwendner@krakenrobotik.de>
2022-01-11 10:24:39 +0000

    added cmp plugins/configuration

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -61,6 +61,18 @@ return packer.startup(function(use)
     run = ':TSUpdate'
   }
 
+  -- cmp plugins
+  use "hrsh7th/nvim-cmp" -- The completion plugin
+  use "hrsh7th/cmp-buffer" -- buffer completions
+  use "hrsh7th/cmp-path" -- path completions
+  use "hrsh7th/cmp-cmdline" -- cmdline completions
+  use "saadparwaiz1/cmp_luasnip" -- snippet completions
+  use "hrsh7th/cmp-nvim-lsp"
+
+  -- snippets
+  use "L3MON4D3/LuaSnip" --snippet engine
+  use "rafamadriz/friendly-snippets" -- a bunch of snippets to use
+
   -- lsp plugins
   use {
     'neovim/nvim-lspconfig',
