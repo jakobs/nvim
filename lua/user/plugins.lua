b1e644057b6fc6469eae7405e2faf7c9ab857731
Jakob Schwendner <jschwendner@krakenrobotik.de>
2022-03-19 13:59:10 +0000

    add lsp-signature plugin

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -75,6 +75,7 @@ return packer.startup(function(use)
   use "saadparwaiz1/cmp_luasnip" -- snippet completions
   use "hrsh7th/cmp-nvim-lsp"
 
+  use "ray-x/lsp_signature.nvim"
   -- snippets
   use "L3MON4D3/LuaSnip" --snippet engine
   use "rafamadriz/friendly-snippets" -- a bunch of snippets to use
